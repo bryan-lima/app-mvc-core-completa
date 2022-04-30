ec2a4b35a5ca4e58f867eed5fa1de56fe69f20a3
Bryan <bryanlima200@outlook.com>
2022-04-30 16:35:27 -0300

    Iniciado criação das services das entidades e desenvolvimento de validações via FluentValidation

---
+++ src/DevIO.Business/Models/Validations/Documentos/ValidacaoDocs.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models.Validations.Documentos
+{
+    public class CpfValidacao
+    {
+        public const int TAMANHO_CPF = 11;
+
+        public static bool Validar(string cpf)
+        {
+            var cpfNumeros = Utils.ApenasNumeros(cpf);
+
+            if (!TamanhoValido(cpfNumeros))
+                return false;
+
+            return !TemDigitosRepetidos(cpfNumeros) && TemDigitosValidos(cpfNumeros);
+        }
+
+        private static bool TamanhoValido(string valor)
+        {
+            return valor.Length == TAMANHO_CPF;
+        }
+
+        private static bool TemDigitosRepetidos(string valor)
+        {
+            string[] invalidNumbers =
+            {
+                "00000000000",
+                "11111111111",
+                "22222222222",
+                "33333333333",
+                "44444444444",
+                "55555555555",
+                "66666666666",
+                "77777777777",
+                "88888888888",
+                "99999999999",
+            };
+
+            return invalidNumbers.Contains(valor);
+        }
+
+        private static bool TemDigitosValidos(string valor)
+        {
+            var number = valor.Substring(0, TAMANHO_CPF - 2);
+
+            var digitoVerificador = new DigitoVerificador(number).ComMultiplicadoresDeAte(2, 11)
+                                                                 .Substituindo("0", 10, 11);
+
+            var firstDigit = digitoVerificador.CalculaDigito();
+            digitoVerificador.AddDigito(firstDigit);
+
+            var secondDigit = digitoVerificador.CalculaDigito();
+
+            return string.Concat(firstDigit, secondDigit) == valor.Substring(TAMANHO_CPF - 2, 2);
+        }
+    }
+
+    public class CnpjValidacao
+    {
+        public const int TAMANHO_CNPJ = 14;
+
+        public static bool Validar(string cpnj)
+        {
+            var cnpjNumeros = Utils.ApenasNumeros(cpnj);
+
+            if (!TemTamanhoValido(cnpjNumeros))
+                return false;
+
+            return !TemDigitosRepetidos(cnpjNumeros) && TemDigitosValidos(cnpjNumeros);
+        }
+
+        private static bool TemTamanhoValido(string valor)
+        {
+            return valor.Length == TAMANHO_CNPJ;
+        }
+
+        private static bool TemDigitosRepetidos(string valor)
+        {
+            string[] invalidNumbers =
+            {
+                "00000000000000",
+                "11111111111111",
+                "22222222222222",
+                "33333333333333",
+                "44444444444444",
+                "55555555555555",
+                "66666666666666",
+                "77777777777777",
+                "88888888888888",
+                "99999999999999"
+            };
+
+            return invalidNumbers.Contains(valor);
+        }
+
+        private static bool TemDigitosValidos(string valor)
+        {
+            var number = valor.Substring(0, TAMANHO_CNPJ - 2);
+
+            var digitoVerificador = new DigitoVerificador(number).ComMultiplicadoresDeAte(2, 9)
+                                                                 .Substituindo("0", 10, 11);
+
+            var firstDigit = digitoVerificador.CalculaDigito();
+            digitoVerificador.AddDigito(firstDigit);
+
+            var secondDigit = digitoVerificador.CalculaDigito();
+
+            return string.Concat(firstDigit, secondDigit) == valor.Substring(TAMANHO_CNPJ - 2, 2);
+        }
+    }
+
+    public class DigitoVerificador
+    {
+        private const int MODULO = 11;
+
+        private string _numero;
+        private readonly List<int> _multiplicadores = new List<int> { 2, 3, 4, 5, 6, 7, 8, 9 };
+        private readonly IDictionary<int, string> _substituicoes = new Dictionary<int, string>();
+        private bool _complementarDoModulo = true;
+
+        public DigitoVerificador(string numero)
+        {
+            _numero = numero;
+        }
+
+        public DigitoVerificador ComMultiplicadoresDeAte(int primeiroMultiplicador, int ultimoMultiplicador)
+        {
+            _multiplicadores.Clear();
+
+            for (var i = primeiroMultiplicador; i <= ultimoMultiplicador; i++)
+                _multiplicadores.Add(i);
+
+            return this;
+        }
+
+        public DigitoVerificador Substituindo(string substituto, params int[] digitos)
+        {
+            foreach (var i in digitos)
+            {
+                _substituicoes[i] = substituto;
+            }
+            return this;
+        }
+
+        public void AddDigito(string digito)
+        {
+            _numero = string.Concat(_numero, digito);
+        }
+
+        public string CalculaDigito()
+        {
+            return !(_numero.Length > 0) ? "" : GetDigitSum();
+        }
+
+        private string GetDigitSum()
+        {
+            var soma = 0;
+            for (int i = _numero.Length - 1, m = 0; i >= 0; i--)
+            {
+                var produto = (int)char.GetNumericValue(_numero[i]) * _multiplicadores[m];
+                soma += produto;
+
+                if (++m >= _multiplicadores.Count) m = 0;
+            }
+
+            var mod = (soma % MODULO);
+            var resultado = _complementarDoModulo ? MODULO - mod : mod;
+
+            return _substituicoes.ContainsKey(resultado) ? _substituicoes[resultado] : resultado.ToString();
+        }
+    }
+
+    public class Utils
+    {
+        public static string ApenasNumeros(string valor)
+        {
+            var onlyNumber = "";
+
+            foreach (var s in valor)
+            {
+                if (char.IsDigit(s))
+                    onlyNumber += s;
+            }
+
+            return onlyNumber.Trim();
+        }
+    }
+}
