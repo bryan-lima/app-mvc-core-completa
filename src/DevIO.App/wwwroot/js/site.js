5855daf051167452bdb56aa02ecb7c213cc71d90
Bryan <bryanlima200@outlook.com>
2022-04-29 22:41:37 -0300

    Adicionado consulta de CEP no ViaCEP utilizando jQuery e incluído uso nas view Create e partial view _AtualizarEndereco de Fornecedores

--- src/DevIO.App/wwwroot/js/site.js
+++ src/DevIO.App/wwwroot/js/site.js
@@ -39,3 +39,63 @@
         }
     });
 }
+
+function BuscaCep() {
+    $(document).ready(function () {
+
+        function LimpaFormularioCep() {
+            // Limpa valores do formulário de CEP
+            $("#Endereco_Logradouro").val("");
+            $("#Endereco_Bairro").val("");
+            $("#Endereco_Cidade").val("");
+            $("#Endereco_Estado").val("");
+        }
+
+        // Quando o campo CEP perde o foco
+        $("#Endereco_Cep").blur(function () {
+            // Nova variável "CEP" somente com dígitos
+            var cep = $(this).val().replace(/\D/g, '');
+
+            // Verifica se campo CEP possui valor informado
+            if (cep != "") {
+                // Expressão regular para validar o CEP
+                var validaCep = /^[0-9]{8}$/;
+
+                // Valida o formato do CEP
+                if (validaCep.test(cep)) {
+                    // Preenche os campos com "..." enquanto consulta webservice
+                    $("#Endereco_Logradouro").val("...");
+                    $("#Endereco_Bairro").val("...");
+                    $("#Endereco_Cidade").val("...");
+                    $("#Endereco_Estado").val("...");
+
+                    // Consulta o webservice viacep.com.br
+                    $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?",
+                        function (dados) {
+                            if (!("erro" in dados)) {
+                                // Atualiza os campos com os valores da consulta
+                                $("#Endereco_Logradouro").val(dados.logradouro);
+                                $("#Endereco_Bairro").val(dados.bairro);
+                                $("#Endereco_Cidade").val(dados.localidade);
+                                $("#Endereco_Estado").val(dados.uf);
+                            }
+                            else {
+                                // CEP pesquisado não foi encontrado
+                                LimpaFormularioCep();
+                                alert("CEP não encontrado");
+                            }
+                        });
+                }
+                else {
+                    // CEP é inválido
+                    LimpaFormularioCep();
+                    alert("Formato de CEP inválido");
+                }
+            }
+            else {
+                // CEP sem valor, limpa formulário
+                LimpaFormularioCep();
+            }
+        });
+    });
+}
